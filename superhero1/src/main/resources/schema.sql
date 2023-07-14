b3c815db0d8c5432ce7dfc29ef718d3c479adf0b
Salma Khairat <salmaakhairat@gmail.com>
2023-07-14 15:22:40 -0400

    general project structure

---
+++ superhero1/src/main/resources/schema.sql
@@ -0,0 +1,43 @@
+DROP DATABASE IF EXISTS superherodb;
+CREATE DATABASE superherodb;
+USE superherodb;
+CREATE TABLE superherovillain(
+	superherovillain_id  INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description TINYTEXT,
+    superpower VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE organization(
+	organization_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description TINYTEXT,
+    org_address MEDIUMTEXT NOT NULL,
+    contact TINYTEXT
+);
+
+CREATE TABLE superhero_organization (
+	superhero_id INT NOT NULL,
+    organization_id INT NOT NULL,
+    PRIMARY KEY(superhero_id, organization_id),
+	FOREIGN KEY (superhero_id) REFERENCES superherovillain(superherovillain_id),
+	FOREIGN KEY (organization_id) REFERENCES organization(organization_id)
+);
+
+CREATE TABLE location (
+	location_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description TINYTEXT,
+    address MEDIUMTEXT NOT NULL,
+    latitude DECIMAL(9,6) NOT NULL,
+    longitute DECIMAL (9,6) NOT NULL
+);
+
+CREATE TABLE sightings (
+	sighting_id INT AUTO_INCREMENT PRIMARY KEY,
+    location_id INT NOT NULL,
+    superhero_id INT NOT NULL,
+    date DATE NOT NULL,
+    FOREIGN KEY (location_id) REFERENCES location(location_id),
+    FOREIGN KEY (superhero_id) REFERENCES superherovillain(superherovillain_id)
+);
